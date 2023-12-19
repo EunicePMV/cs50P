4b0c67f705347d2ee86fbc2ad7dbc5816e3d29ab
EunicePMV <66046122+EunicePMV@users.noreply.github.com>
2023-12-19 08:40:13 +0000

    Tue, Dec 19, 2023, 4:40 PM +08:00

--- faces/faces.py
+++ faces/faces.py
@@ -1,7 +1,10 @@
 def convert(user_input):
     # convert the emoticon here
-    user_input.replace(':)', str(':)'))
+    user_input.replace(':)', '🙂')
     print(user_input)
+
+    # if there is ':)' replace the emoticon
+    
 
 def main():
     user_input = input('Give me a word/sentence: ')

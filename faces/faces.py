cded7f51f63ea7ebeab8c76220c1e24281f317df
EunicePMV <66046122+EunicePMV@users.noreply.github.com>
2023-12-20 02:53:52 +0000

    Wed, Dec 20, 2023, 10:53 AM +08:00

--- faces/faces.py
+++ faces/faces.py
@@ -1,7 +1,7 @@
 def convert(user_input):
     if ':)' in user_input and ':(' in user_input:
-        user_input.replace(':)', '🙂')
-        user_input.replace(':(', '🙁')
+        user_input = user_input.replace(':)', '🙂')
+        user_input = user_input.replace(':(', '🙁')
         print(user_input)
     elif ':)' in user_input:
         print(user_input.replace(':)', '🙂'))

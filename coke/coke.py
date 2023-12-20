50296ba1a3e256c5bf95678e241286ef1a86d1b2
EunicePMV <66046122+EunicePMV@users.noreply.github.com>
2023-12-20 05:35:35 +0000

    Wed, Dec 20, 2023, 1:35 PM +08:00

--- coke/coke.py
+++ coke/coke.py
@@ -3,13 +3,12 @@
 # then minus and print
 
 amount_due = 50
-change = 0
-while amount_due != 0:
+while amount_due > 0:
     print('Amount Due:' + amount_due)
     user_input = int(input('Insert a coin: '))
     if user_input == 25 or user_input == 10 or user_input == 5:
         amount_due = coke - user_input
-        if
     else:
         continue
 
+print('Change Owed: ', abs(amount_due))

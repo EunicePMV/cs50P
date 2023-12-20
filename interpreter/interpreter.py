0fedf48f191effa420bbe3a27374b6d80a21c29d
EunicePMV <66046122+EunicePMV@users.noreply.github.com>
2023-12-20 03:47:56 +0000

    Wed, Dec 20, 2023, 11:47 AM +08:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -1,13 +1,13 @@
 user_input = input('Give me a math expression: ')
 x, y, z = user_input.split(' ')
 x = float(x)
-y = float(y)
+z = float(z)
 match y:
     case '+':
-        print(x + y)
+        print(x + z)
     case '-':
-        print(x - y)
+        print(x - z)
     case '*':
-        print(x * y)
+        print(x * z)
     case '/':
-        print(x / y)
+        print(x / z)

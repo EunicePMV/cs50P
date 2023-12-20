6939c6fc57ef13dde5edf0495a45e86c5eec8fb0
EunicePMV <66046122+EunicePMV@users.noreply.github.com>
2023-12-20 04:49:44 +0000

    Wed, Dec 20, 2023, 12:49 PM +08:00

--- meal/meal.py
+++ meal/meal.py
@@ -2,8 +2,12 @@ def main():
     user_input = input('Give me a time: ')
     hours = convert(user_input)
 
-    if hours 
-
+    if hours > 7 and hours < 8:
+        print('breakfast time')
+    elif hours > 12 and hours < 13:
+        print('lunch time')
+    elif hours > 18 and hours < 19:
+        print('dinner time')
 
 def convert(time):
     hours, minutes = time.split(':')

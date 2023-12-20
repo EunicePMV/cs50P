c8a79a1250a8b81b25bdf664eb1c9500d638ab24
EunicePMV <66046122+EunicePMV@users.noreply.github.com>
2023-12-20 08:03:45 +0000

    Wed, Dec 20, 2023, 4:03 PM +08:00

--- plates/plates.py
+++ plates/plates.py
@@ -1,2 +1,17 @@
 # must start with atleast 2 letters
-# 
+# n >= 2 and n <= 6
+#
+
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+    ...
+
+
+main()

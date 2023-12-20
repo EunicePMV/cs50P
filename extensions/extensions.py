f2f91db8e7e818972f1d16fa66c88d33f03c1f31
EunicePMV <66046122+EunicePMV@users.noreply.github.com>
2023-12-20 03:38:03 +0000

    Wed, Dec 20, 2023, 11:38 AM +08:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -2,7 +2,7 @@ user_input = input('Give me a file name: ').lower().strip()
 
 if '.gif' in user_input:
     print('image/gif')
-elif '.jpeg' or '.jpg' in user_input:
+elif '.jpeg' in user_input or '.jpg' in user_input:
     print('image/jpeg')
 elif '.png' in user_input:
     print('image/png')

3e9f663bd95c94440dedc41b9ccf306ee552bf83
EunicePMV <66046122+EunicePMV@users.noreply.github.com>
2023-12-20 02:49:36 +0000

    Wed, Dec 20, 2023, 10:49 AM +08:00

--- faces/faces.py
+++ faces/faces.py
@@ -1,19 +1,11 @@
 def convert(user_input):
-    if user_input.find(':)'):
-        user_input.replace
-        user_input[user_input.find(':)')] = '🙂'
-        user_input[user_input.find(':)') + 1] = ''
-    elif user_input.find(':('):
-        user_input[user_input.find(':(')] = '🙁'
-        user_input[user_input.find(':(') + 1] = ''
-
+    if ':)' in user_input:
+        print(user_input.replace(':)', '🙂'))
+    elif ':(' in user_input:
+        print(user_input.replace(':(', '🙁'))
 
 def main():
     user_input = input('Give me a word/sentence: ')
     convert(user_input)
 
-# main()
-
-
-user_input = input('Give me a word/sentence: ')
-print(user_input.replace(':)', '🙂'))
+main()

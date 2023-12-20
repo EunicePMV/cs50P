348ff0fb540918d5e23fcc15cfa217e6c547b84b
EunicePMV <66046122+EunicePMV@users.noreply.github.com>
2023-12-20 03:10:22 +0000

    Wed, Dec 20, 2023, 11:10 AM +08:00

--- tip/tip.py
+++ tip/tip.py
@@ -1,8 +1,10 @@
 def main():
     dollars = dollars_to_float(input("How much was the meal? "))
+    print(dollars)
     percent = percent_to_float(input("What percentage would you like to tip? "))
-    tip = dollars * percent
-    print(f"Leave ${tip:.2f}")
+    print(percent)
+    # tip = dollars * percent
+    # print(f"Leave ${tip:.2f}")
 
 
 def dollars_to_float(d):
@@ -12,7 +14,7 @@ def dollars_to_float(d):
 
 def percent_to_float(p):
     p = p.replace('%', '')
-    return float(p)
+    return float(p) * 0.01
 
 
 main()

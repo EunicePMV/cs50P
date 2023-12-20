f1bf05465d2dadb433f01ccb1568a9e1e36ff193
EunicePMV <66046122+EunicePMV@users.noreply.github.com>
2023-12-20 03:33:49 +0000

    Wed, Dec 20, 2023, 11:33 AM +08:00

---
+++ extensions/extensions.py
@@ -0,0 +1,8 @@
+user_input = input('Give me a file name: ').lower().strip()
+
+if '.gif' in user_input:
+    print('image/gif')
+elif '.jpeg' or '.jpg' in user_input:
+    print('image/jpeg')
+elif
+    

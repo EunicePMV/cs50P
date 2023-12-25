9a3831989fa0e0d5c2bc5c31891f3325c316aaac
EunicePMV <66046122+EunicePMV@users.noreply.github.com>
2023-12-25 08:28:01 +0000

    Mon, Dec 25, 2023, 4:28 PM +08:00

--- plates/plates.py
+++ plates/plates.py
@@ -12,15 +12,15 @@ def is_valid(s):
     if len(s) >= 2 and len(s) <= 6:
         if special_char in s:
             return False
-
         for pos, letter in enumerate(s):
             if letter.isdigit():
                 if letter == '0':
                     return False
                 substring = s[pos:]
                 break
-
         if substring.isdigit():
             return True
+    else:
+        return False
 
 main()

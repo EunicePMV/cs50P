101595d8519c0b6b64b1d980e81b9534a4907a1f
EunicePMV <66046122+EunicePMV@users.noreply.github.com>
2024-01-04 06:47:41 +0000

    Thu, Jan 4, 2024, 2:47 PM +08:00

--- taqueria/grocery/grocery.py
+++ taqueria/grocery/grocery.py
@@ -1,9 +1,9 @@
-grocery_lst = []
+grocery_lst = {}
 
 while True:
     try:
         item = input()
-        grocery_lst.append(item)
+        grocery_lst[item] = 1
     except EOFError:
         grocery_lst.sort()
         for item in grocery_lst:

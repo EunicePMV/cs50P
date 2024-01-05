cf12dfda9ba9c9c904ab23294bcf5fa33946738d
EunicePMV <66046122+EunicePMV@users.noreply.github.com>
2024-01-05 09:19:01 +0000

    Fri, Jan 5, 2024, 5:19 PM +08:00

--- professor/professor.py
+++ professor/professor.py
@@ -5,22 +5,24 @@ def main():
     # should prompt 1,2, or 3; else reprompt
     lvl = get_level()
 
+    generate_integer(lvl)
+
     # output EEE if not correct or not even a number, prompt the user again until 3 tries, the program should output the correct answer
     # output the scores
-    scores = 0
-    trials = 3
-    for _ in range(10):
-        # generate 10 math probs, X and Y random positive int: X + Y =
-        X, Y = generate_integer(lvl)
-        total = X+Y
+    # scores = 0
+    # trials = 3
+    # for _ in range(10):
+    #     # generate 10 math probs, X and Y random positive int: X + Y =
+    #     X, Y = generate_integer(lvl)
+    #     total = X+Y
 
-        # given 3 trials for each problem
-        trials = 3
-        while trials != 0:
-            sum = input(f'{X} + {Y} = ')
-            if sum == X+Y:
-                print('correct')
-                continue
+    #     # given 3 trials for each problem
+    #     trials = 3
+    #     while trials != 0:
+    #         sum = input(f'{X} + {Y} = ')
+    #         if sum == X+Y:
+    #             print('correct')
+    #             continue
 
 
 
@@ -36,6 +38,7 @@ def generate_integer(level):
     # generate non-negative int with level digits or raises ValueError if level not 1, 2, or 3
     X = random.randint(1, level)
     Y = random.randint(1, level)
+    print(X, Y)
     return X, Y
 
 

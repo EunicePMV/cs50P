57599fafdbc99810a2bd1209095a8d2553b52fce
EunicePMV <66046122+EunicePMV@users.noreply.github.com>
2024-01-05 09:04:53 +0000

    Fri, Jan 5, 2024, 5:04 PM +08:00

--- professor/professor.py
+++ professor/professor.py
@@ -6,22 +6,21 @@ def main():
     lvl = get_level()
 
     # generate 10 math probs, X and Y random positive int: X + Y =
-    X, Y = generate_integer(lvl)
+    # X, Y = generate_integer(lvl)
 
     # output EEE if not correct or not even a number, prompt the user again until 3 tries, the program should output the correct answer
     # output the scores
 
 
 def get_level():
-    valid_lvl = [1, 2, 3]
-    done = False
-    while not done:
-        lvl = int(input('Level: '))
-        if lvl in valid_lvl:
-            return lvl
+    while True:
+        lvl = input('Level: ')
+        if lvl.isnumeric() and lvl != '0':
+            return int(lvl)
 
 
 def generate_integer(level):
+    valid_lvl = [1, 2, 3]
     # generate non-negative int with level digits or raises ValueError if level not 1, 2, or 3
     ...
 

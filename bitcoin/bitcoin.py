f655ee4a471c129bb42ea1e0ab8bda4e7d6b6c76
EunicePMV <66046122+EunicePMV@users.noreply.github.com>
2024-01-05 09:45:18 +0000

    Fri, Jan 5, 2024, 5:45 PM +08:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -1,6 +1,14 @@
-import requests
+import requests, sys
 
 try:
     ...
 except requests.RequestException:
     ...
+except IndexError:
+    sys.exit('Missing command-line argument')
+
+
+# command-line argument the number of Bitcoins n
+    # convert to float, if not sys.exit
+# query the current price of bitcoin as a float
+# cost of n bitcoins in USD to four decimal palce using ,

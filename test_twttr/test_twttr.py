52ef3f9d47394005ea232be5e4c4a0d4de18f298
EunicePMV <66046122+EunicePMV@users.noreply.github.com>
2024-01-08 06:06:33 +0000

    Mon, Jan 8, 2024, 2:06 PM +08:00

---
+++ test_twttr/test_twttr.py
@@ -0,0 +1,26 @@
+from twttr import shorten
+
+def test_lower_vowels():
+    assert shorten('love') == 'lv'
+    assert shorten('yabiu') == 'yb'
+
+def test_upper_vowels():
+    assert shorten('LOVE') == 'LV'
+    assert shorten('YABIU') == 'YB'
+
+# def main():
+#     test_twttr()
+
+# def test_twttr():
+#     vowel = 'aeiouAEIOU'
+#     if shorten('love') != 'lv':
+#         print('love should be lv')
+#     if shorten('yabiu') != 'yb':
+#         print('yab should be yb')
+#     if shorten('LOVE') != 'LV':
+#         print('LOVE should be LV')
+#     if shorten('YABIU') != 'YB':
+#         print('YABIU should be YB')
+
+# if __name__ == "__main__":
+#     main()
---
+++ test_twttr/twttr.py
@@ -0,0 +1,16 @@
+def main():
+    word = input('Input:')
+    print(shorten(word))
+
+def shorten(word):
+    new_word = ''
+    vowel = 'AEIOU'
+    for letter in word:
+        if letter in vowel:
+            continue
+        new_word += letter
+
+    return new_word
+
+if __name__ == "__main__":
+    main()

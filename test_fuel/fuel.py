2d7f9c2d27e77e50756d122e7c1556675761877b
EunicePMV <66046122+EunicePMV@users.noreply.github.com>
2024-02-12 05:29:50 +0000

    Mon, Feb 12, 2024, 1:29 PM +08:00

--- test_fuel/fuel.py
+++ test_fuel/fuel.py
@@ -2,7 +2,11 @@ class InvalidNumerator(Exception):
     pass
 
 def main():
-    ...
+    done = False
+    while not done:
+        try:
+            fraction = input('Fraction: ')
+            num, denom = fraction.split('/')
 
 
 def convert(fraction):
@@ -16,29 +20,29 @@ def gauge(percentage):
 if __name__ == "__main__":
     main()
 
-done = False
-while not done:
-    try:
-        fraction = input('Fraction: ')
-        num, denom = fraction.split('/')
-        num = int(num)
-        denom = int(denom)
+# done = False
+# while not done:
+#     try:
+#         fraction = input('Fraction: ')
+#         num, denom = fraction.split('/')
+#         num = int(num)
+#         denom = int(denom)
 
-        if num > denom:
-            raise InvalidNumerator
+#         if num > denom:
+#             raise InvalidNumerator
 
-        percent = round(num/denom*100)
+#         percent = round(num/denom*100)
 
-        if percent >= 99:
-            print('F')
-        elif percent <= 1:
-            print('E')
-        else:
-            print(f'{percent}%')
-        done = True
-    except ValueError:
-        continue
-    except ZeroDivisionError:
-        continue
-    except InvalidNumerator:
-        continue
+#         if percent >= 99:
+#             print('F')
+#         elif percent <= 1:
+#             print('E')
+#         else:
+#             print(f'{percent}%')
+#         done = True
+#     except ValueError:
+#         continue
+#     except ZeroDivisionError:
+#         continue
+#     except InvalidNumerator:
+#         continue

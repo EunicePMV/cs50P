33d94e0b0b1bcc19e42cd9b154eadc15882b8fbf
EunicePMV <66046122+EunicePMV@users.noreply.github.com>
2024-02-12 03:17:18 +0000

    Mon, Feb 12, 2024, 11:17 AM +08:00

--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -1,2 +1,13 @@
 from bank import value
 
+def test_hello():
+    assert value('hello') == '$0'
+
+def test_h():
+    assert value('hi') == '$20'
+
+def test_other_greetings():
+    assert value('good morning') == '$100'
+
+def test_capitalize_greetings():
+    assert value('GOOD MORNING') == '$100'

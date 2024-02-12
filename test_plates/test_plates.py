5932c58d5732958d7a0c4976667d891b1354f0ba
EunicePMV <66046122+EunicePMV@users.noreply.github.com>
2024-02-12 04:05:47 +0000

    Mon, Feb 12, 2024, 12:05 PM +08:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -16,4 +16,4 @@ def test_num_middle():
 def test_alphanumeric():
     assert is_valid('123') == False
     assert is_valid('BSCS') == True
-    assert is_vaLid('CS50') == True
+    assert is_valid('CS50') == True

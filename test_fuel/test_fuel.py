4f6f013bd72d73b89f8da6734d4e026e3a75e0ad
EunicePMV <66046122+EunicePMV@users.noreply.github.com>
2024-02-12 05:58:42 +0000

    Mon, Feb 12, 2024, 1:58 PM +08:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -1,4 +1,10 @@
+import pytest
 from fuel import convert, gauge
 
-def test_zero_division():
-    assert '100/0' == 
+def test_zero_division_error():
+    with pytest.raises(ZeroDivisionError):
+        1/0
+
+def test_value_error():
+    with pytest.raises(ValueError):
+        

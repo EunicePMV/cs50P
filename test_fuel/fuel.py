8d1a2800fe5990020e56ffb5bcd87e970e960bb5
EunicePMV <66046122+EunicePMV@users.noreply.github.com>
2024-02-12 05:31:38 +0000

    Mon, Feb 12, 2024, 1:31 PM +08:00

--- test_fuel/fuel.py
+++ test_fuel/fuel.py
@@ -2,15 +2,36 @@ class InvalidNumerator(Exception):
     pass
 
 def main():
-    done = False
-    while not done:
-        try:
-            fraction = input('Fraction: ')
-            num, denom = fraction.split('/')
+    fraction = input('Fraction: ')
+    convert(fraction)
+
 
 
 def convert(fraction):
-    ...
+    try:
+        num, denom = fraction.split('/')
+        num = int(num)
+        denom = int(denom)
+
+        if num > denom:
+            raise InvalidNumerator
+
+        percent = round(num/denom*100)
+
+        if percent >= 99:
+            print('F')
+        elif percent <= 1:
+            print('E')
+        else:
+            print(f'{percent}%')
+        done = True
+    except ValueError:
+        continue
+    except ZeroDivisionError:
+        continue
+    except InvalidNumerator:
+        continue
+
 
 
 def gauge(percentage):

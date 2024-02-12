69d36f8d632818d8fd52c6db7419c5b36164e78a
EunicePMV <66046122+EunicePMV@users.noreply.github.com>
2024-02-12 06:05:20 +0000

    Mon, Feb 12, 2024, 2:05 PM +08:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -5,3 +5,10 @@ def test_zero_division_error():
     with pytest.raises(ZeroDivisionError) as error:
         convert('100/0')
     assert error.type is ZeroDivisionError
+
+def test_value_error():
+    with pytest.raises(ValueError) as error:
+        convert('one/two')
+    assert error.type is ValueError
+
+

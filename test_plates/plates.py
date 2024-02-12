db7ebc5eb2561c4c987be9745a062ff65caab86b
EunicePMV <66046122+EunicePMV@users.noreply.github.com>
2024-02-12 03:24:28 +0000

    Mon, Feb 12, 2024, 11:24 AM +08:00

---
+++ test_plates/plates.py
@@ -0,0 +1,30 @@
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+    special_char = '. [@_!$%^&*()<>?/\\|}{~:]#'
+    substring = ''
+    if len(s) >= 2 and len(s) <= 6:
+        if s.isalpha():
+            return True
+        elif s.isdigit() and len(s) == 2:
+            return False
+
+        for pos, letter in enumerate(s):
+            if letter.isdigit():
+                if letter == '0':
+                    return False
+                elif s[pos:].isdigit():
+                    return True
+            elif letter in special_char:
+                return False
+    else:
+        return False
+
+if __name__ == "__main__":
+    main()

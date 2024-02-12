ae15998678eabe5e8a5da78a2ec251c422f79d92
EunicePMV <66046122+EunicePMV@users.noreply.github.com>
2024-02-12 03:35:57 +0000

    Mon, Feb 12, 2024, 11:35 AM +08:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -1,11 +1,11 @@
 from plates import is_valid
 
+def test_beginning_alphabet():
+    assert is_valid('')
+
 def test_min_max_characters():
     assert is_valid('S') == False
     assert is_valid('YABIEEE') == False
-
-# def test_two_alphabets():
-#     assert is_valid('CS50') == True
 
 def test_leading_zero():
     assert is_valid('CS05') == False

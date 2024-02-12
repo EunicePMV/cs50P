4a2fb5d32a361f51228995536819996c4c3dbb39
EunicePMV <66046122+EunicePMV@users.noreply.github.com>
2024-02-12 03:08:19 +0000

    Mon, Feb 12, 2024, 11:08 AM +08:00

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -9,26 +9,9 @@ def test_upper_vowels():
     assert shorten('YABIU') == 'YB'
 
 def test_number():
-    assert shorten('love123') == 'lv'
-    assert shorten('yabiu08') == 'yb'
+    assert shorten('love123') == 'lv123'
+    assert shorten('yabiu08') == 'yb08'
 
-# def test_punctuation():
-#     assert shorten('l,o,v,e') == 'lv'
-#     assert shorten('yabiu~!@#$%^&*(),./;"[]|') == 'yb'
-
-# def main():
-#     test_twttr()
-
-# def test_twttr():
-#     vowel = 'aeiouAEIOU'
-#     if shorten('love') != 'lv':
-#         print('love should be lv')
-#     if shorten('yabiu') != 'yb':
-#         print('yab should be yb')
-#     if shorten('LOVE') != 'LV':
-#         print('LOVE should be LV')
-#     if shorten('YABIU') != 'YB':
-#         print('YABIU should be YB')
-
-# if __name__ == "__main__":
-#     main()
+def test_punctuation():
+    assert shorten('l,o,v,e') == 'l,,v,'
+    assert shorten('yabiu~!@#$%^&*(),./;"[]|') == 'yb~!@#$%^&*(),./;"[]|'

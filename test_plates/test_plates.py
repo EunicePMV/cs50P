1c34fef8183f96cd694237b72da617eabad869f8
EunicePMV <66046122+EunicePMV@users.noreply.github.com>
2024-02-12 03:30:02 +0000

    Mon, Feb 12, 2024, 11:30 AM +08:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -1,3 +1,14 @@
-from plates import isvalid
+from plates import is_valid
 
-def test 
+def min_max_characters():
+    assert is_valid('CS') == False
+    assert is_valid('YABIEEE') == False
+
+def test_two_alphabets():
+    assert is_valid('CS50') == True
+
+def test_leading_zero():
+    assert is_valid('CS05') == False
+
+def test_num_middle():
+    assert is_valid('CS50P') == False

5eb4c5279a3ccc115d8bcba6578f5f7a77125f00
EunicePMV <66046122+EunicePMV@users.noreply.github.com>
2024-02-13 03:44:22 +0000

    Tue, Feb 13, 2024, 11:44 AM +08:00

---
+++ scourgify/scourgify.py
@@ -0,0 +1,36 @@
+import sys
+
+def main():
+    try:
+        file = sys.argv[1]
+
+        try:
+            second_arg = sys.argv[2]
+            if second_arg:
+                sys.exit('Too many command-line arguments')
+        except IndexError:
+            if not file.endswith('.py'):
+                sys.exit('Not a Python file')
+
+        try:
+            with open(file, 'r') as f:
+                lines = f.readlines()
+
+            num_lines = 0
+            for line in lines:
+                line = line.lstrip(' ')
+                if line.startswith('#'):
+                    continue
+                elif line == '\n':
+                    continue
+                num_lines += 1
+            print(num_lines)
+
+        except FileNotFoundError:
+            sys.exit('File does not exist')
+
+    except IndexError:
+        sys.exit('Too few command-line arguments')
+
+if __name__ == "__main__":
+    main()

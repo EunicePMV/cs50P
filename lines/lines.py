4aea6c375191dc451ea3e357b485e913b4e9f0a7
EunicePMV <66046122+EunicePMV@users.noreply.github.com>
2024-02-13 02:27:15 +0000

    Tue, Feb 13, 2024, 10:27 AM +08:00

---
+++ lines/lines.py
@@ -0,0 +1,8 @@
+# expects one command-line argument:
+#   - the name or path of a Python file
+#   -> output: num of lines
+
+# if file:
+#   - does not end in .py
+#   - file does not exist
+# exit: sys.exit

e4ee78fb8e6c871d43b72d141a5721301979bd50
EunicePMV <66046122+EunicePMV@users.noreply.github.com>
2024-02-13 07:09:12 +0000

    Tue, Feb 13, 2024, 3:09 PM +08:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -16,17 +16,11 @@ def main():
             elif os.path.splitext(file)[1] != os.path.splitext(second_file)[1]:
                 sys.exit('Input and output have different extension')
 
-        print('success')
-
         try:
-            print('success')
+            shirt = Image.open("shirt.png")
+            shirt_size = shirt.size
 
-            # shirt = Image.open("shirt.png")
-            # shirt_size = shirt.size
-
-            # muppet = Image.open(file)
-            # muppet = muppet.crop(shirt_size)
-            # muppet.save('crop.jpg')
+            muppet = Image.open(file)
 
             # crop the muppet pict according to the shirt size
 

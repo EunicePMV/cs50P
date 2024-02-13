55ba1a534d9a5f4be6a49e43b909158fb26a2a1c
EunicePMV <66046122+EunicePMV@users.noreply.github.com>
2024-02-13 02:32:51 +0000

    Tue, Feb 13, 2024, 10:32 AM +08:00

--- lines/lines.py
+++ lines/lines.py
@@ -7,8 +7,11 @@
 #   - file does not exist
 # exit: sys.exit
 
+import sys
+
 def main():
     # get the user input in the command-line
+    print(sys.argv[1])
 
 if __name__ == "__main__":
     main()

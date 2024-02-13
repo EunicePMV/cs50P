ef9f6e0d29ffebcc5b78c824076bc1ed8a257392
EunicePMV <66046122+EunicePMV@users.noreply.github.com>
2024-02-13 06:36:55 +0000

    Tue, Feb 13, 2024, 2:36 PM +08:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -21,11 +21,7 @@ def main():
             shirt = Image.open("shirt.png")
             shirt_size = shirt.size
 
-            shirt = erode(12, shirt)
-
             muppet = Image.open(file)
-            muppet_size = muppet.size
-            print(muppet_size)
 
             muppet.paste(shirt, shirt_size)
             muppet.save(second_file)

c9eb8b1b16827d8fd19d7f304c4ddb53a156f1eb
EunicePMV <66046122+EunicePMV@users.noreply.github.com>
2024-02-14 02:18:45 +0000

    Wed, Feb 14, 2024, 10:18 AM +08:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -1,6 +1,16 @@
 import re
 import sys
 
+# .   any character except a new line
+# *   0 or more repetitions
+# +   1 or more repetitions
+# ?   0 or 1 repetition
+# {m} m repetitions
+# {m,n} m-n repetitions
+# ^   matches the start of the string
+# $   matches the end of the string or just before the newline at the end of the string
+# []    set of characters
+# [^]   complementing the set
 
 def main():
     print(validate(input("IPv4 Address: ")))

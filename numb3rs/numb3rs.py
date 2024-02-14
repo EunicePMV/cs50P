333512be8d23f8582e9fb23b2549c2a309d87c5b
EunicePMV <66046122+EunicePMV@users.noreply.github.com>
2024-02-14 02:16:47 +0000

    Wed, Feb 14, 2024, 10:16 AM +08:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -6,6 +6,11 @@ def main():
     print(validate(input("IPv4 Address: ")))
 
 
+# in the ip address should be the following:
+#   - ranging from 0-255
+#   - only has 3 dots (255.255.255.255)
+#   - only has numbers
+#   - 
 def validate(ip):
     ...
 

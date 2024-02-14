f5335d7c5c61b3fe9f4b3177ac7e1592b240c469
EunicePMV <66046122+EunicePMV@users.noreply.github.com>
2024-02-14 04:06:03 +0000

    Wed, Feb 14, 2024, 12:06 PM +08:00

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -1,1 +1,18 @@
-# two or more functions
+from numb3rs import validate
+
+def test_characters():
+    assert validate('cat') == False
+    assert validate('cat.cat.cat.cat') == False
+
+
+def test_special_characters():
+    assert validate('255,255,255,255') == False
+    assert validate('255/255/255/255') == False
+    assert validate('255;255;255;255') == False
+
+def test_range_num():
+    assert validate('512.512.512.512') == False
+    assert validate('275.28.1.10') == False
+
+def test_length_address():
+    assert validate('255.255.255.255.255') == False

e0c1c5121fbf7de1e8886a4961ba3551ded560c8
EunicePMV <66046122+EunicePMV@users.noreply.github.com>
2024-02-14 04:01:33 +0000

    Wed, Feb 14, 2024, 12:01 PM +08:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -21,21 +21,11 @@ import sys
 def main():
     print(validate(input("IPv4 Address: ").strip()))
 
-
-# in the ip address should be the following:
-#   - ranging from 0-255
-#   - only has 3 dots (255.255.255.255)
-#   - only has numbers
-#   - no other special characters other than "dot"
 def validate(ip):
     if re.search(r"^[0-255]+\.[0-255]+\.[0-255]+\.[0-255]$", ip):
         return True
     else:
         return False
-
-
-...
-
 
 if __name__ == "__main__":
     main()

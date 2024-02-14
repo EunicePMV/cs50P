d247485af4b233add82d67cb930e8631c82fde52
EunicePMV <66046122+EunicePMV@users.noreply.github.com>
2024-02-14 04:23:56 +0000

    Wed, Feb 14, 2024, 12:23 PM +08:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -22,7 +22,7 @@ def main():
     print(validate(input("IPv4 Address: ").strip()))
 
 def validate(ip):
-    # ^[0-256]+\.[0-255]+\.[0-255]+\.[0-255]$ -> first trial
+    # ^[0-255]+\.[0-255]+\.[0-255]+\.[0-255]$ -> first trial
     if re.search(r"^[0-255]\.+[0-255]\.+[0-255]\.+[0-255]", ip):
         return True
     else:

4bb4022b517ea24dfa15656065d6b33513549ba9
EunicePMV <66046122+EunicePMV@users.noreply.github.com>
2024-02-14 07:39:28 +0000

    Wed, Feb 14, 2024, 3:39 PM +08:00

--- watch/watch.py
+++ watch/watch.py
@@ -1,6 +1,23 @@
 import re
 import sys
 
+# .   any character except a new line
+# *   0 or more repetitions
+# +   1 or more repetitions
+# ?   0 or 1 repetition
+# {m} m repetitions
+# {m,n} m-n repetitions
+# ^   matches the start of the string
+# $   matches the end of the string or just before the newline at the end of the string
+# []    set of characters
+# [^]   complementing the set
+# \d    decimal digit
+# \D    not a decimal digit
+# \s    whitespace characters
+# \S    not a whitespace character
+# \w    word character, as well as numbers and the underscore
+# \W    not a word character
+
 
 def main():
     print(parse(input("HTML: ")))
@@ -9,7 +26,7 @@ def main():
 def parse(s):
     s = s.strip()
     # get the src link
-    matches = re.search(r"^(src)=\"(.)\"$", s, re.IGNORECASE)
+    matches = re.search(r"^(<iframe \w) (src=\"http\")", s, re.IGNORECASE)
     matches.group()
 
 

0fddd902957ec6d940f61a77d25866d01a3185cb
EunicePMV <66046122+EunicePMV@users.noreply.github.com>
2024-02-19 11:25:17 +0000

    Mon, Feb 19, 2024, 7:25 PM +08:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -9,14 +9,14 @@ def get_mins(birthdate):
     try:
         # check if input is valid
         birthdate = date.fromisoformat(birthdate)
-        current_date = date.today()
+        # current_date = date.today()
+        current_date = date.fromisoformat('2000-01-01')
         age = current_date - birthdate
-        print(age)
-        # total_mins = round(age.days * 24 * 60)
+        total_mins = round(age.days * 24 * 60)
 
-        # p = inflect.engine()
-        # word_mins = p.number_to_words(total_mins)
-        # print(f'{word_mins} minutes')
+        p = inflect.engine()
+        word_mins = p.number_to_words(total_mins)
+        print(f'{word_mins} minutes')
     except ValueError:
         sys.exit('Invalid date')
 

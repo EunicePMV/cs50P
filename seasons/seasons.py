c1486deace5d388e483231fab011a5f73be2432f
EunicePMV <66046122+EunicePMV@users.noreply.github.com>
2024-02-19 10:57:34 +0000

    Mon, Feb 19, 2024, 6:57 PM +08:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -3,14 +3,14 @@ import sys, inflect
 
 
 def main():
-    get_mins(input('Date of Birth: ')))
+    get_mins(input('Date of Birth: '))
 
 def get_mins(birthdate):
     try:
         # check if input is valid
         date.fromisoformat(birthdate)
     except ValueError:
-        return "Invalid date"
+        sys.exit('Invalid date')
 
 
 

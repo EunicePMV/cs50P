f5b0bcc48bbd765b645f316558403f563e05306b
EunicePMV <66046122+EunicePMV@users.noreply.github.com>
2024-02-19 10:29:19 +0000

    Mon, Feb 19, 2024, 6:29 PM +08:00

--- um/test_um.py
+++ um/test_um.py
@@ -1,3 +1,15 @@
 from um import count
 
-def test_
+def test_only_word():
+    assert count("um") == 1
+    assert count(" um ") == 1
+
+def test_word_between():
+    assert count("thank um, a lot") == 1
+    assert count("hello, um world") == 1
+
+def test_case_sensitive():
+    assert count("Um, you okay?") == 1
+
+def test_with_um_word():
+    assert count("this is your album!") == 0

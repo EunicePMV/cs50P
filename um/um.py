335d70ad9ab592f82d57676da30e6b8eeddae747
EunicePMV <66046122+EunicePMV@users.noreply.github.com>
2024-02-19 10:26:04 +0000

    Mon, Feb 19, 2024, 6:26 PM +08:00

--- um/um.py
+++ um/um.py
@@ -8,10 +8,6 @@ def main():
 
 def count(s):
     s = s.lower()
-    # invalid: yummy, yum, album
-    # valid: ' um ', 'um', 'um.,?!'
-    # 1st pattern: \s?um[\s\.\,\?\!]*?[^\w]
-    # match the string if walang katabing character
     sub_pattern = r"\bum\b"
     found = re.findall(sub_pattern, s)
     return len(found)

ba04d87cff1cd748bb7aaaff84ac580b632c54e7
EunicePMV <66046122+EunicePMV@users.noreply.github.com>
2024-02-19 10:50:57 +0000

    Mon, Feb 19, 2024, 6:50 PM +08:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -2,10 +2,12 @@ from datetime import date
 
 
 def main():
-    ...
+    print(get_mins(input('Date of Birth: ')))
 
+def get_mins(birthdate):
+    # check if input is valid
+    
 
-...
 
 
 if __name__ == "__main__":

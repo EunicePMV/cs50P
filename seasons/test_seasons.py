7e08e85717c9b4b39ea18c108022993158663117
EunicePMV <66046122+EunicePMV@users.noreply.github.com>
2024-02-19 12:09:15 +0000

    Mon, Feb 19, 2024, 8:09 PM +08:00

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -5,9 +5,10 @@ from datetime import date
 TEST_DATE = "2000-01-01"
 
 def test_valid_date():
-    with pytest.raises(ValueError) as error:
+    with pytest.raises(SystemExit) as error:
         get_mins("January 01, 1999", TEST_DATE)
-    assert error.type is ValueError
+    assert error.type is SystemExit
+    assert str(error.value) == "Invalid date"
 
 def test_word_mins():
     get_mins("1999-01-01", TEST_DATE) == "Five hundred twenty-five thousand, six hundred minutes"

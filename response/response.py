0a9744e88400c8bab1e7724bfb27ad48e77473ac
EunicePMV <66046122+EunicePMV@users.noreply.github.com>
2024-02-19 10:37:01 +0000

    Mon, Feb 19, 2024, 6:37 PM +08:00

--- response/response.py
+++ response/response.py
@@ -1,11 +1,15 @@
-from validator_collection import validators
+from validator_collection import validators, errors
 
 def main():
     print(validate(input("What's your email address? ")))
 
 
 def validate(s):
-    
+    try:
+        valid = validators.email(s)
+        return valid
+    except ValueError or errors.EmptyValueError:
+        return False
 
 
 if __name__ == "__main__":

64e565ca9473d3145f78d01856066bcbdbe9ee31
EunicePMV <66046122+EunicePMV@users.noreply.github.com>
2024-02-20 10:38:32 +0000

    Tue, Feb 20, 2024, 6:38 PM +08:00

--- jar/test_jar.py
+++ jar/test_jar.py
@@ -1,8 +1,15 @@
+import pytest
 from jar import Jar
 
 
 def test_init():
-    ...
+    with pytest.raises(ValueError) as error:
+        jar = Jar(-1)
+    assert error.type is ValueError
+
+    with pytest.raises(ValueError) as error:
+        jar = Jar("cookies")
+    assert error.type is ValueError
 
 
 def test_str():
@@ -15,8 +22,17 @@ def test_str():
 
 
 def test_deposit():
-    ...
+    jar = Jar()
+    jar.deposit(1)
+    assert jar.size == 1
+    jar.deposit(2)
+    assert jar.size == 3
 
 
 def test_withdraw():
-    ...
+    jar = Jar()
+    jar.deposit(12)
+    jar.withdraw(2)
+    assert jar.size == 10
+    jar.withdraw(2)
+    assert jar.size == 8

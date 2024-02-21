6d4d41667720519abf96e07de3370d12dc9029d4
EunicePMV <66046122+EunicePMV@users.noreply.github.com>
2024-02-21 09:22:51 +0000

    Wed, Feb 21, 2024, 5:22 PM +08:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -4,8 +4,8 @@ from fpdf import FPDF
 # name = input("Name: ")
 pdf = FPDF(orientation="P", format="A4")
 pdf.add_page()
-pdf.set_title("CS50 Shirtificate")
-# pdf.set_font("helvetica", size=12)
+pdf.set_font("helvetica", size=12)
+pdf.cell(0, 10, "CS50 Shirtificate")
 # # add center text for header
 # pdf.image("shirtificate.png")
 pdf.output("shirtificate.pdf")
